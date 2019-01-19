f74635a689203f00d05df5f7bbf8fd990884b239
limdongjin <geniuslim27@gmail.com>
2019-01-19 20:05:32 +0900

    1.0.4-alpha.3

--- demo/.vuepress/config.js
+++ demo/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: 'simple seo plugin demo',
     description: 'Welcome Contributes',
-    plugins: [ require('./plugins/my-plugin'), {
+    plugins: [ '@limdongjin/vuepress-plugin-simple-seo', {
         default_image: '/image.jpg',
         root_url: 'https://limdongjin.github.io',
         default_site_name: 'limdongjin TIL',
